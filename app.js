07e4bb395c4b5823d77a378564c4fcaf661bb1fa
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-03-23 17:15:22 +0530

    Update Socket.io connection URL to use production server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Connect to Socket.io server
-const socket = io('http://localhost:5000');
+const socket = io('https://realtimechat-4rew.onrender.com');
 
 // DOM Elements
 const loginScreen = document.getElementById('loginScreen');
